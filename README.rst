08aa65f7c1f74e16675e9d9f3350427f84b21a6f
liato <x@x00.us>
2010-03-11 03:31:26 +0100

    Added interactive switch

--- README.rst
+++ README.rst
@@ -1,11 +1,21 @@
 gReminder
 =========
 
+
+Requirements
+------------
+
+* A Google account and a verified phone number in Google Calendar
+* `gdata-python-client <http://code.google.com/p/gdata-python-client/>`_
+* `parsedatetime <http://code-bear.com/code/parsedatetime/>`_
+
+
+
 Usage
 -----
 
 1. Copy config.py.example to config.py and enter your username and password.
-2. python gremind.py or ./gremind.py
+2. python gremind.py eat pizza in 2h 30m or python gremind.py -i for interactive mode
 3. ????
 4. Get an SMS reminder from Google.
 
--- gremind.py
+++ gremind.py
@@ -13,6 +13,7 @@ import sys
 import string
 import time
 import calendar
+from optparse import OptionParser
 
 try:
     from config import username, password
@@ -23,6 +24,37 @@ except ImportError:
     
 from parsedatetime import parsedatetime as pdt    
 
+
+def print_help():
+    print 'Usage: python %s <title> in <time>' % sys.argv[0]
+    print 'Example: python %s eat pizza in 2h 30m' % sys.argv[0]
+    sys.exit()
+
+parser = OptionParser()
+parser.usage = "%prog [[options] | <reminder> in <time>]\nExample: %prog eat pizza in 2h 30m"
+parser.add_option("-i", "--interactive", dest="interactive",
+                  action="store_true", help="Run gRemind in interactive mode")
+
+options, args = parser.parse_args()
+
+if len(sys.argv) < 2:
+    parser.print_help()
+    sys.exit()
+
+if options.interactive:
+    title = raw_input('Remind me to: ')
+    when = raw_input('In: ')
+else:
+    args = ' '.join(sys.argv[1:])
+    if ' in ' in args:
+        i = args.rfind(' in ')
+        title = args[:i]
+        when = args[i+4:]
+    else:
+        print_help()
+        
+title = title.decode(sys.stdout.encoding or 'utf-8', 'replace')
+
 gcal = gdata.calendar.service.CalendarService()
 gcal.email = username
 gcal.password = password
@@ -30,9 +62,6 @@ gcal.source = 'gRemind'
 gcal.ProgrammaticLogin()
 dateparser = pdt.Calendar()
 
-title = raw_input('Remind me to: ')
-title = title.decode(sys.stdout.encoding or 'utf-8', 'replace')
-when = raw_input('In: ')
 start_time = dateparser.parse(when, time.gmtime())[0]
 end_time = time.gmtime(calendar.timegm(start_time)+60)
 start_time = time.strftime('%Y-%m-%dT%H:%M:%S.000Z', start_time)
